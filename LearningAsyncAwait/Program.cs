c49c4ebb1c993f526d9f01f8dec59b6e3b693b80
Nikolai Semenov <n.semenov@atol.ru>
2018-05-18 21:02:25 +0300

    First

--- LearningAsyncAwait/Program.cs
+++ LearningAsyncAwait/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LearningAsyncAwait
@@ -10,6 +11,32 @@ namespace LearningAsyncAwait
     {
         static void Main(string[] args)
         {
+            DisplayResultAsync();
+            Console.ReadLine();
+        }
+
+        static async void DisplayResultAsync()
+        {
+            int num = 5;
+
+            int result = await FactorialAsync(num);
+            Thread.Sleep(3000);
+            Console.WriteLine("Факториал числа {0} равен {1}", num, result);
+        }
+
+        static Task<int> FactorialAsync(int x)
+        {
+            int result = 1;
+
+            return Task.Run(() =>
+            {
+                for (int i = 1; i <= x; i++)
+                {
+                    result *= i;
+                }
+                return result;
+            });
         }
     }
+
 }
